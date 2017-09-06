0ef4e7478f58f5ff8f0131a89025c5d9d6f5b834
James Thomas <codeactor@protonmail.com>
2017-09-06 19:13:39 +0100

    initial commit

---
+++ pypinp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+import subprocess
+import os
+import sys
+import re
+
+
+MPV = ['mpv',
+       '--ontop',
+       '--no-border',
+       '--on-all-workspaces',
+       '--autofit=640x320',
+       '--geometry=98%:98%'
+       ]
+
+
+STREAM_URL = '--ytdl-format (mp4)[height<=1080]/best[height<=1080]'
+
+
+
+def play_local(filename):
+    MPV.append(filename)
+    subprocess.run(MPV)
+
+
+def play_url(url):
+    MPV.append(STREAM_URL)
+    MPV.append(url)
+    subprocess.run(MPV)
+
+
+def main(argv):
+    to_play = argv[0]
+
+    if os.path.isfile(to_play):
+        play_local(to_play)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
