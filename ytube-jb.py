683ca9783fa08490fa1502264838197d4086f9ff
James Thomas <codeactor@protonmail.com>
2017-09-10 12:19:23 +0100

    initial commit

---
+++ ytube-jb.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+
+import subprocess
+import os
+import sys
+import re
+import argparse
+
+
+DEFAULT_NUMBER = 5
+
+
+def get_args(args):
+    parser = argparse.ArgumentParser(description='YouTube as a command line jukebox')
+    parser.add_argument('source', type=str, help='file or url to play')
+    parser.add_argument('-n', '--number-to-play', type=int, help='number of streams to play', metavar='')
+    return parser.parse_args(args)
+ 
+
+class PlayAudio:
+
+    def __init__(self, source, source_type, number_to_play):
+        self.source = source
+        self.source_type = source_type
+        self.number_to_play = number_to_play
+        self.play_audio()
+
+    def play_url(self):
+        subprocess.run(['mpv', '--no-video', self.source])
+
+    def play_search_result(self):
+        pass
+
+    def play_audio(self):
+        play = {'url': self.play_url,
+                'search': self.play_search_result}
+        play[self.source_type]()
+
+
+def main(argv):
+    args = get_args(argv)
+
+    source = args.source
+
+    if re.match(r'^http', source):
+        source_type = 'url'
+    else:
+        source = 'search'
+
+    if args.number_to_play:
+        number_to_play = args.number_to_play
+    else:
+        number_to_play = DEFAULT_NUMBER
+
+    PlayAudio(source, source_type, number_to_play)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
